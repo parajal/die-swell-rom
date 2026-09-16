d9ae94a1e4d1bf52bb5d865c280f1c3cf5186fc8
prai <parajalrai@gmail.com>
2026-09-16 13:31:10 +0200

    readme changes

--- romlab/online_phase.py
+++ romlab/online_phase.py
@@ -12,43 +12,24 @@ class OnlinePhase:
         self.predictions = {}
         self.errors = {}
         self.mean_errors = {}
-        self.stds = {}
 
-    def reconstruct_sol(self, field=None, X=None, return_std=False):
-        """Reconstruct every trained field, or only `field` (str or list)."""
-        if field is None:
-            field = list(self.offline.models)
-        fields = [field] if isinstance(field, str) else list(field)
-
-        if X is None:
-            X = self.fom.parameters_test[:, self.fom.param_cols]
-        X = np.atleast_2d(X)
+    def reconstruct_sol(self):        
+        X = self.fom.parameters_test[:, self.fom.param_cols]
         if self.offline.scaler is not None:
             X = self.offline.scaler.transform(X)
 
-        for fld in fields:
-            coeffs, std = self.offline.models[fld].predict(X, return_std=True)
-
-            # sklearn drops the mode axis when there is a single mode
-            coeffs = np.reshape(coeffs, (len(X), -1))
-            std = np.broadcast_to(np.reshape(std, (len(X), -1)), coeffs.shape).copy()
-            pred = coeffs @ self.pod.data[fld]["basis"].T + + self.pod.data[fld]["lifting"]
-
+        for fld in self.pod.computed_fields:
+            coeffs = self.offline.models[fld].predict(X, return_std=False)
+            pred = coeffs @ self.pod.data[fld]["basis"].T + self.pod.data[fld]["lifting"]
             truth = getattr(self.fom, f"{self.pod.FIELDS[fld]}_test")
-            rel = (np.linalg.norm(pred - truth, axis=1) / np.linalg.norm(truth, axis=1)
-                   if pred.shape == truth.shape else None)
-
+            rel = np.linalg.norm(pred - truth, axis=1) / np.linalg.norm(truth, axis=1)
             self.predictions[fld] = pred
             self.errors[fld] = rel
-            self.mean_errors[fld] = None if rel is None else float(rel.mean())
-            self.stds[fld] = std
+            self.mean_errors[fld] = rel.mean()
 
-        if isinstance(field, str):
-            preds, stds = self.predictions[field], self.stds[field]
-        else:
-            preds = {f: self.predictions[f] for f in fields}
-            stds = {f: self.stds[f] for f in fields}
-        return (preds, stds) if return_std else preds
+        preds = {f: self.predictions[f] for f in self.pod.computed_fields}
+
+        return preds
 
     def info(self):
         for fld, rel in self.errors.items():
